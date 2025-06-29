00803d6bd15e936620b52a4eb514ef859b4db275
karimerosu <fujitaka4916@icloud.com>
2025-06-29 17:36:21 +0900

    Create game.h

---
+++ game.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+
+void add_score(Game box[], int size);
+void printcore(Game box[], int size);
+void outputscore(Game box[], int size);
+void throwdarts(const char *filename);
+void startgame(void);
+
+typedef struct game{
+    int score;
+    int throw_number[3];
+    int frame;
+}Game;
