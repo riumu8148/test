3e2ffb27520ec3de3d1c5b70b6ddfc7e0b286ea4
riumu8148 <nakaketamm@gmail.com>
2025-06-29 14:14:34 +0900

    throw

---
+++ throw.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"throw.h"
+
+void throw(int argc,char *argv[]){
+    FILE *fp;
+    if((fp=fopen(argv[1],'r'))==NULL){
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+    fclose(fp);
+    return 0;
+}
---
+++ throw.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void throw_darts(int ,char*[]);
