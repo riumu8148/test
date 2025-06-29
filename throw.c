61a3ae089b3352e6b21c68fb6319ce5a050c4d6b
riumu8148 <nakaketamm@gmail.com>
2025-06-29 17:25:00 +0900

    change

--- throw.c
+++ throw.c
@@ -2,12 +2,13 @@
 #include<stdlib.h>
 #include"throw.h"
 
-void throw(int argc,char *argv[]){
-    FILE *fp;
+void throw_darts(char *argv[]){
+
     if((fp=fopen(argv[1],'r'))==NULL){
         perror("fopen");
         exit(EXIT_FAILURE);
     }
-    fclose(fp);
+
     return 0;
+    
 }
--- throw.h
+++ throw.h
@@ -1,3 +1,6 @@
 #pragma once
+#include<stdio.h>
+
+extern FILE *fp;
 
 void throw_darts(int ,char*[]);
