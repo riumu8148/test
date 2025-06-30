747fec35feed2dc1eff1a4c2df5f88227fccd871
karimerosu <fujitaka4916@icloud.com>
2025-06-30 19:52:03 +0900

    Update throw.c

--- throw.c
+++ throw.c
@@ -1,14 +1,30 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include"throw.h"
+#include "game.h"
 
-void throw(int argc,char *argv[]){
-    FILE *fp;
-    if((fp=fopen(argv[1],'r'))==NULL){
-        perror("fopen");
-        exit(EXIT_FAILURE);
+extern FILE *fp;
+extern Game box[8];
+char check[1024];
+
+void throw(char *inputfile)
+{
+    int i = 0;
+
+    if ((fp = fopen(inputfile, "r")) == NULL)
+    {
+        printf("読み込みファイルなし");
+        exit(1);
     }
-
-    return 0;
-    
+    while (fgets(check, sizeof(check), fp) != NULL)
+    {
+        int j = 0;
+        strtok(check, ',');
+        while (check != NULL)
+        {
+            box[i].score[j] = atoi(check);
+            box[i].frame = atoi(i);
+            strtok(NULL, ',');
+            j++;
+        }
+        i++;
+    }
+    fclose(fp);
 }
