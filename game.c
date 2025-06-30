9a54ea91bf1dcf43c43c04928319d22d746def79
karimerosu <fujitaka4916@icloud.com>
2025-07-01 00:04:26 +0900

    Update game.c

--- game.c
+++ game.c
@@ -3,10 +3,10 @@ Game box[8];
 FILE *fp = NULL;
 
 int main(int argc, char *argv[]){
-    startgame();    //game開始をアナウンスする
+    //startgame();    //game開始をアナウンスする
     throwdarts(argv[1]);      //fileopenを確認する
     add_score(box,8);    //スコアを計算する
-    printcore(box,8);    //スコアを表示する
+    printscore(box,8);    //スコアを表示する
     outputscore(box,8);  //スコアを出力する
     return 0;
 }
