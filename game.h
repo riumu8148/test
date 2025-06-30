be4fdacd9c12a148905823fd8d6f0d96860e311e
riumu8148 <nakaketamm@gmail.com>
2025-06-30 21:57:42 +0900

    Update game.h

--- game.h
+++ game.h
@@ -3,14 +3,14 @@
 #include<stdlib.h>
 #include<string.h>
 
-void add_score(Game box[], int size);
-void printcore(Game box[], int size);
-void outputscore(Game box[], int size);
-void throwdarts(const char *filename);
-void startgame(void);
-
 typedef struct game{
     int score;
     int throw_number[3];
     int frame;
 }Game;
+
+void add_score(Game box[], int size);
+void printcore(Game box[], int size);
+void outputscore(Game box[], int size);
+void throwdarts(const char *filename);
+void startgame(void);
